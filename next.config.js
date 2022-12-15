53a18511634df7af883177699315e9fe325a7541
FrazaAdityaWiguna <adityafraja12@gmail.com>
2022-12-15 21:34:48 +0700

    fix: edit next config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  pageExtensions: ["store.js"],
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
